710e4d17d4002b6c3337f35c5037e34703f543cd
Andreas092025 <andreas.092026@gmail.com>
2026-04-23 09:56:59 +0200

    Small fixed DataImport.cs

--- DigimonDB/Data/DataImport.cs
+++ DigimonDB/Data/DataImport.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 
 namespace DigimonDB.Data;
 
@@ -9,12 +10,12 @@ public record MoveDto(string Name, string Type, int Power, string Description);
 public record ItemDto
 {
     [JsonPropertyName("item")]
-    public string Name { get; init; }
+    public string Name { get; init; } = string.Empty;
     
-    public string Type { get; init; }
+    public string Type { get; init; } = string.Empty;
     
     [JsonPropertyName("materials")]
-    public string[] Effect { get; init; }
+    public string[] Effect { get; init; } = Array.Empty<string>();
 }
 
 public record CharacterDto(string Name, string Role, string Description);
@@ -22,14 +23,15 @@ public record CharacterDto(string Name, string Role, string Description);
 public record DataDto
 {
     [JsonPropertyName("digimons")]
-    public DigimonDto[] Digimons { get; init; }
+    public DigimonDto[] Digimons { get; init; } = Array.Empty<DigimonDto>();
     
     [JsonPropertyName("moves")]
-    public MoveDto[] Moves { get; init; }
+    public MoveDto[] Moves { get; init; } = Array.Empty<MoveDto>();
     
     [JsonPropertyName("items")]
-    public ItemDto[] Items { get; init; }
+    public ItemDto[] Items { get; init; } = Array.Empty<ItemDto>();
+    
     
     [JsonPropertyName("characters")]
-    public CharacterDto[] Characters { get; init; }
+    public CharacterDto[] Characters { get; init; } = Array.Empty<CharacterDto>();
 }
